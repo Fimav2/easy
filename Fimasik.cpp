0e208f228cde8129a6007ce28244c07e9d7de885
Fimav2 <113976105+Fimav2@users.noreply.github.com>
2022-09-26 17:34:26 +0300

    Romanov Efim

---
+++ Fimasik.cpp
@@ -0,0 +1,18 @@
+#include"ff.h"
+
+#include<iostream>
+using namespace std;
+
+void itc_name(){
+    cout << "Fima" << endl;
+}
+void itc_fio(){
+    cout << "Romanov Efim" << endl;
+}
+int itc_abs(int num){
+if (num>=0)
+     return num;
+return (-1 * num);
+
+}
+
---
+++ ff.h
@@ -0,0 +1,13 @@
+#ifndef FF_H_INCLUDED
+#define FF_H_INCLUDED
+
+#include<iostream>
+
+void itc_name();
+
+void itc_fio();
+
+int itc_abs(int num);
+
+
+#endif // FF_H_INCLUDED
